c44bc05dc198c221f4b99737f5082f2168eaaf93
jcha-ultra <john@ultratesting.us>
2021-10-18 14:56:35 -0400

    minor cleanup

--- R/run_testfile.R
+++ R/run_testfile.R
@@ -1,12 +1,12 @@
-# runs a single testfile and returns results
+# run a single testfile and returns results
 run_testfile <- function(path, test_list_name = "test_list") {
   test_list <- (function() {source(path, local = TRUE); environment()})()[[test_list_name]] # retrieves test_list variable from testfile # nolint
   test_results <- list()
   for (test_name in names(test_list)) {
-    print(paste0("---- test started:", test_name, "----"))
+    print(paste0("----test started: ", test_name, "----"))
     run_test <- test_list[[test_name]]
     test_results[[test_name]] <- run_test()
-    print(paste("---- test complete:", test_name, "----"))
+    print(paste0("----test complete: ", test_name, "----"))
   }
   return(test_results)
 }
