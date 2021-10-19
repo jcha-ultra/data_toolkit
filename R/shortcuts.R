844e3993031e095528223ae23ca5770c5bd39ff8
jcha-ultra <john@ultratesting.us>
2021-10-19 12:21:35 -0400

    more cleanup

--- R/shortcuts.R
+++ R/shortcuts.R
@@ -11,7 +11,7 @@ find_names_in_zip <- function(zip_file, pattern, ...) {
 str_includes_2_way <- function(x, y) grepl(x, y) || grepl(y, x)
 
 # find all elements in `a` that has an equivalent in `b`, with equivalence as defined by `eq`. This is NOT commutative, as it will only return the version of the element found in `a`. Do not use for large sets, as it is O(n^2)
-# for more comprehensive and performant functionality, use the `fuzzyjoin` package: https://cran.r-project.org/web/packages/fuzzyjoin/index.html
+# for more comprehensive and performant functionality, use the `fuzzyjoin` package: https://cran.r-project.org/package=fuzzyjoin
 fuzzy_intersect <- function(a, b, eq) {
   # check if an element `x` is in `b`
   x_in_b <- function(x) purrr::reduce(b, ~.x || eq(.y, x), .init = FALSE)
