02c8e8cf98e262e0672a81ee0ffdff0f5a47b836
jcha-ultra <15233508+jcha-ultra@users.noreply.github.com>
2021-12-03 11:45:39 -0500

    cleanup

--- R/minimal_neural_net_example.R
+++ R/minimal_neural_net_example.R
@@ -1,5 +1,5 @@
 # minimal example of a neural net
 library("neuralnet")
-training_data = read.csv("~/repos/toolkit/datasets/roots.csv", header=TRUE)
-nn_model <- neuralnet(formula = root_k ~ k, data = training_data, hidden = 10, threshold = 0.01)
-output <- cbind(training_data, nn_output = unlist(nn_model$net.result))
+training_data <- read.csv("~/repos/toolkit/datasets/roots.csv", header = TRUE)
+model <- neuralnet(formula = root_k ~ k, data = training_data, hidden = 10)
+output <- cbind(training_data, nn_output = unlist(model$net.result))
