29026a3c73a628823305ea6da7ac40b42af3ea28
jcha-ultra <15233508+jcha-ultra@users.noreply.github.com>
2022-04-22 05:31:39 -0400

    add some more minor functions

--- python/minor.py
+++ python/minor.py
@@ -1,7 +1,6 @@
-
-from typing import Any, Tuple
+from typing import Any, Tuple, Callable
 import numpy as np
-import datetime
+import pandas as pd
 
 def is_number(n: Any) -> bool:
     """Check if value is convertible to a number."""
@@ -16,7 +15,22 @@ def is_ten_power(x, y) -> bool:
     if is_number(x) and is_number(y) and float(x)!=0 and float(y)!=0:
         return np.log10(np.abs(float(x)/float(y)))%1 == 0
 
-def is_timezone_offset(dt1: datetime.datetime, dt2: datetime.datetime) -> 'Tuple[bool, int]':
-    """Check if one datetime is separated by another by a integer number of hours, and less than a day, and returns the offset. Accurate to the second."""
-    diff = dt1 - dt2
-    return (diff.components.days == 0 and diff.components.minutes == 0 and diff.components.seconds == 0), diff.components.hours
+def is_datetime(dt: Any) -> bool:
+    """Check if value is convertible to a datetime."""
+    try:
+        pd.to_datetime(dt)
+        return True
+    except ValueError:
+        return False
+
+def is_timezone_offset(dt1: pd.Timestamp, dt2: pd.Timestamp) -> 'Tuple[bool, int]':
+    """Check if one datetime is separated by another by a integer number of hours, and less than a day, and returns the offset."""
+    diff = dt1.replace(tzinfo=None) - dt2.replace(tzinfo=None)
+    hours_diff = diff.total_seconds() / 3600
+    is_offset = hours_diff % 1 == 0 and np.abs(hours_diff) < 24
+    return is_offset
+# test_is_timezone_offset = is_timezone_offset(pd.to_datetime('2020-01-01T00:00:00+00:00'), pd.to_datetime('2020-01-01T00:00:00+00:00'))
+
+def is_same_up_to(x: float, y: float, n: int) -> bool:
+    """Check if two numbers are the same up to a certain number of decimal places."""
+    return np.round(x, n) == np.round(y, n)
