da03fb51c1335aef54f2342647dd095a9f00ff34
jcha-ultra <15233508+jcha-ultra@users.noreply.github.com>
2022-10-15 14:35:27 -0400

    add dataclass destructuring

--- python_toolkit/classes.py
+++ python_toolkit/classes.py
@@ -4,3 +4,11 @@ class Foo:
     def __init__(*args, **kwargs):
         super().__init__(*args, **kwargs)
 
+# destructuring dataclasses
+from dataclasses import astuple, dataclass
+@dataclass
+class Foo:
+    bar: str
+    baz: int
+foo = Foo('bar', 42)
+bar, baz = astuple(foo)
