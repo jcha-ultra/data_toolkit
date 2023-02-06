7a5fea1b900799cb9b6fb21f9367929b8aaf6ce0
jcha-ultra <15233508+jcha-ultra@users.noreply.github.com>
2023-02-05 21:30:21 -0500

    add demo function

--- ml/autocoder.py
+++ ml/autocoder.py
@@ -99,6 +99,39 @@ def draft_function(name: str, functionality: str, assistant: Assistant) -> Draft
     )
 
 
+def demo():
+    """Demo the function drafting process using ."""
+    from langchain.llms import OpenAI
+
+
+    # Set up the OpenAI API key in the environment, if needed
+    # os.environ["OPENAI_API_KEY"] = "<your key>"
+
+    # If the key is not set, the code will raise an error
+    if "OPENAI_API_KEY" not in os.environ:
+        raise ValueError("Please set the OpenAI API key as an environment variable.")
+
+
+    function_name = "prime"
+    functionality = "Generate the nth prime number. This function is efficient for large values of n."
+
+    # name = "fibonacci"
+    # functionality = "generates the nth fibonacci number"
+
+    # name = "get_python_architecture_structure"
+    # functionality = "Given a directory of Python packages and modules, create a dictionary that describes the structure of the packages, modules, classes, and functions within the directory."
+
+    model_name = "text-davinci-003"
+    text_llm = OpenAI(temperature=0, model_name=model_name, max_tokens=-1)
+    assistant = BasicOpenAIAssistant(text_llm)
+    results = draft_function(
+        name=function_name, functionality=functionality, assistant=assistant
+    )
+    func_draft = results.code
+    print(func_draft)
+    breakpoint()
+
+
 # os.environ["OPENAI_API_KEY"] = "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"
 if __name__ == "__main__":
     demo()
